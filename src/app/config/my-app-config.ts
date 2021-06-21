b1b4095d995792ca2aa7d0102cfef674da094996
Debrup Mondal <57451228+debrupofficial365@users.noreply.github.com>
2021-06-21 22:17:30 +0530

    #bug fixed

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
 
     oidc:{
-        clientId:'0oa6qnm2v1CKdtbGJ5d6',
+        clientId:'################',
         issuer: 'https://dev-60747921.okta.com/oauth2/default',
         redirectUri:'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
